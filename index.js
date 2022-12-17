ff2dbff2b7c8f70a84c8d3b4d9102192a9a77e11
Francsico Monti <montifranciscojavier@gmail.com>
2022-12-16 21:28:22 -0300

    desafio 02 coder backend

--- index.js
+++ index.js
@@ -1,104 +1,128 @@
-const fs = require('fs');
+const fs = require("fs");
 
-class ProductManager{
-    constructor(pathFile){
-        this.path = pathFile;
+class ProductManager {
+    constructor(path) {
+        this.path = path;
         this.products = this.readFile();
     }
 
-    readFile(){
-        const data = JSON.parse(fs.readFileSync(`./${this.path}`, 'utf-8'))
+    readFile() {
+        const data = JSON.parse(fs.readFileSync(`./${this.path}`, "utf-8"));
         return data;
     }
 
-    writeData(data){
+    writeData(data) {
         let dataString = JSON.stringify(data);
         fs.writeFileSync(`./${this.path}`, dataString);
         return dataString;
     }
 
-    idGenerator(){
-        if(this.products.length > 0){
-            let id = this.products.map(product => product.id)
-            return Math.max(...id) + 1
-        }else{
-            let id = 1
-            return id
-        }
+    idGenerator() {
+        if (this.products.length > 0) {
+            let productsId = this.products.map((product) => product.id);
+            return Math.max(...productsId) + 1;
+        } 
+
+        let id = 1;
+        return id;
     }
-    
-    getAllProducts(){
+
+    getAllProducts() {
         let data = this.readFile();
         console.log(data);
         return data;
     }
 
-    addProduct(product){
-        if(this.products.find(item => item.code ===  product.code)){
+    addProduct(product) {
+
+        if (this.products.find((item) => item.code === product.code)) {
             return console.log("Product code already exists");
-        }
-
-        else if(product.title == null || product.price == null || product.code == null || product.description == null || product.thumbnail == null || product.stock == null){
+        } else if (
+            !!!product.title ||
+            !!!product.price ||
+            !!!product.code ||
+            !!!product.description ||
+            !!!product.thumbnail ||
+            !!!product.stock
+        ) {
             return console.log("some date is null");
-        }
-
-        else{
-            let data = this.readFile();
-            product.id = this.idGenerator();
-            data.push(product);
-            this.writeData(data);
-        }
+        } 
+        
+        let data = this.readFile();   
+        product.id = this.idGenerator();
+        data.push(product);
+        this.writeData(data);
+        
     }
 
     getProductById(id) {
-
         let data = this.readFile();
-        if(data.find(product => product.id === id)){
-            let productToGet = data.find(product => product.id === id)
-            console.log(productToGet)
+        let productToGet = data.find((product) => product.id === id);
+        if (productToGet) {
+            console.log(productToGet);
             return productToGet;
-        }else{
-            console.log("The id of the product to get is not found")
         }
+
+        console.log("The id of the product to get is not found");
     }
 
-    updateProduct(id, product){
+    updateProduct(id, product) {
         let data = this.readFile();
-        if(data.find(product => product.id === id)){
-            let productDeleted = data.filter(product => product.id !== id)
+        if (data.find((product) => product.id === id)) {
+            let productDeleted = data.filter((product) => product.id !== id);
             product.id = id;
             productDeleted.push(product);
             this.writeData(productDeleted);
             return productDeleted;
-        }else{
-            console.log("The id of the product to update is not found")
         }
+
+        console.log("The id of the product to update is not found");
     }
 
-    deleteProduct(id){
+    deleteProduct(id) {
         let data = this.readFile();
-        if(data.find(product => product.id === id)){
-            let products = data.filter(product => product.id !== id)
+        if (data.find((product) => product.id === id)) {
+            let products = data.filter((product) => product.id !== id);
             this.writeData(products);
-            console.log(products)
+            console.log(products);
             return products;
-        }else{
-            console.log("The id of the product to delete is not found")
         }
+
+        console.log("The id of the product to delete is not found");
     }
-
-
 }
 
+const productManager = new ProductManager("products.json");
 
-const productManager = new ProductManager('products.json')
+//Testing
 
-let product1 = { title:'Pencil',description: 'Black pencil', price: 150 , code : 'd213123ada', thumbnail: 'url://images', stock : 10 }
+let product1 = {
+    title: "Pencil",
+    description: "Black pencil",
+    price: 150,
+    code: "dsada",
+    thumbnail: "url://images",
+    stock: 10,
+};
 
-productManager.addProduct(product1)
+//-----Add new product --------
+// productManager.addProduct(product1);
 
-productManager.getProductById(2)
 
-productManager.deleteProduct(1)
+//-----Get product by id--------
+// productManager.getProductById(3);
 
-productManager.updateProduct(4, { title:'tshirt',description: 'Black tshirt', price:300 , code : 'asdas', thumbnail: 'url://images', stock : 10 })
+
+//-----Delete product by id --------
+//productManager.deleteProduct(3);
+
+
+//-----Update exist product --------
+// productManager.updateProduct(2, {
+//      title: "tshirt",
+//      description: "Black tshirt",
+//      price: 300,
+//      code: "asdas",
+//      thumbnail: "url://images",
+//     stock: 10
+//  });
